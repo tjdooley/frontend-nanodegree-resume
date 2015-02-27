19f76f3b35583834062d93132924a26b4080adac
tjdooley <tjdooley@gmail.com>
2015-02-26 22:32:25 -0600

    Test

---
+++ js/resumeBuilder.js
@@ -0,0 +1,45 @@
+var work = {
+  jobs : []
+};
+
+work.jobs.push({
+  employer : "UW Health",
+  title : "Senior Web Developer",
+  location : "Madison",
+  dates : "October 2013 - Present",
+  description : ""
+});
+
+var projects = {
+  projects : []
+};
+
+projects.projects.push({
+  title : "Photo Release Gallery",
+  dates : "October 2013 - July 2014",
+  description : "",
+  images : []
+});
+
+var bio = {
+  name : "TJ Dooley",
+  role : "Senior Web Developer",
+  welcomeMessage : "Welcome",
+  contacts : {
+  	mobile : "608-443-6064",
+  	email : "tjdooley@gmail.com"
+  },
+  skills : []
+};
+
+var education = {
+  schools : [{
+  	name : "UW Madison",
+  	location : "Madison",
+  	degree : "BS",
+  	majors : ["Computer Science"],
+  	dates : "August 1998 - May 2003",
+  	url : "http://www.wisc.edu"
+  }],
+  onlineCourses : []
+};
